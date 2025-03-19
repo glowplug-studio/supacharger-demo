58c1d65c3aba2c08bc94a79e44d0c1f8eacf867a
j@sharpi.sh <j@sharpi.sh>
2025-03-19 18:55:08 +0700

    playing with auth dashboard switching content on root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,36 @@ import Image from 'next/image';
 import Link from 'next/link';
 import ExampleDash from '@/supacharger/components/exampleDash';
 import ExampleHero from '@/supacharger/components/exampleHero';
+import { AccountMenu } from '@/components/account-menu';
+import { signOut } from './(auth)/auth-actions';
+
 import { useTranslations } from 'next-intl';
 
-export default function RootPage() {
+import { isLoggedIn } from '@/utils/supabase/server';
+
+
+
+export default async function RootPage() { 
+
+  const session = await isLoggedIn();
 
   return (
-    <div className='flex flex-col gap-8 lg:gap-32'>
-      <HeroSection />
-    </div>
-  );
-}
 
-function HeroSection() {
-
-  const t = useTranslations('AuthedFeed')
-
-  return (
     <section className='relative overflow-hidden lg:overflow-visible bg-gray-800'>
+      
+      {session ? (<>
+        <ExampleDash></ExampleDash>
+        <AccountMenu signOut={signOut} />
+        </>
+      ) : (
+        <>
+          <ExampleHero></ExampleHero>
+        </>
+      )}
 
-      <ExampleDash></ExampleDash>
 
-      <ExampleHero></ExampleHero>
-
-    </section>
+  </section>
   );
+
 }
+
