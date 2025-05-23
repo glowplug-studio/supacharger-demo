f6be4aa40cd2d621a9d22d730ca4a6d62ae6b2a1
j@sharpi.sh <j@sharpi.sh>
2025-05-24 03:29:23 +1200

    taking out more garbage

--- src/app/(supacharger)/(unauthenticated)/account/reset-password/new/page.tsx
+++ src/app/(supacharger)/(unauthenticated)/account/reset-password/new/page.tsx
@@ -9,7 +9,7 @@ import { toast } from 'react-toastify';
 import InlineLoader from '@/assets/images/ui/InlineLoader.svg';
 import SCSiteLogo from '@/components/sc_demo/sc_site-logo';
 import SaveButton from '@/supacharger/components/buttons/form-save-button';
-import { createClient } from '@/supacharger/lib/supabase/client';
+import { createClient } from '@/supacharger/lib/client';
 import { supabaseErrorCodeLocalisation } from '@/supacharger/utils/helpers';
 
 export default function LoginPage() {
