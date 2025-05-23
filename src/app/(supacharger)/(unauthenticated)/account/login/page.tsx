b93dfa3a831ecdb5740a08d67adf8196bcb3f3b8
j@sharpi.sh <j@sharpi.sh>
2025-05-24 02:10:55 +1200

    fixed that stupid login error

--- src/app/(supacharger)/(unauthenticated)/account/login/page.tsx
+++ src/app/(supacharger)/(unauthenticated)/account/login/page.tsx
@@ -16,11 +16,9 @@ export default function LoginPage() {
     <>
       {/* Mobile only logo */}
       <div className='flex items-center gap-2 lg:hidden'>
-          <div className='w-6'>
-            <Link href='/'>
-              <SCSiteLogo showSiteTitle={true} darkMode={false} />
-            </Link>
-          </div>
+        <div className='w-6'>
+          <SCSiteLogo showSiteTitle={true} darkMode={false} />
+        </div>
       </div>
 
       <div className='flex flex-col gap-2'>
@@ -29,49 +27,44 @@ export default function LoginPage() {
 
       <LoginUserForm></LoginUserForm>
 
-       <UIDivider></UIDivider>
-      
+      <UIDivider></UIDivider>
 
       {/* Social Buttons */}
 
       <div className='flex flex-col gap-2'>
-              <button className='flex w-full items-center justify-between rounded-lg border border-gray-700 bg-gray-700 px-4 py-3 transition-colors hover:border-gray-500'>
-                <span className='font-medium text-gray-100'>Continue with Google</span>
-                <img
-                  src='https://d36se1z1wvp1u2.cloudfront.net/img/assets/social-media/google.svg'
-                  alt='Google'
-                  className='h-6 w-6'
-                />
-              </button>
-              <button className='flex w-full items-center justify-between rounded-lg border border-gray-700 bg-gray-700 px-4 py-3 transition-colors hover:border-gray-500'>
-                <span className='font-medium text-gray-100'>Continue with Facebook</span>
-                <img
-                  src='https://d36se1z1wvp1u2.cloudfront.net/img/assets/social-media/facebook.svg'
-                  alt='Facebook'
-                  className='h-6 w-6'
-                />
-              </button>
-              <button className='flex w-full items-center justify-between rounded-lg border border-gray-700 bg-gray-700 px-4 py-3 transition-colors hover:border-gray-500'>
-                <span className='font-medium text-gray-100'>Continue with Discord</span>
-                <img
-                  src='https://d36se1z1wvp1u2.cloudfront.net/img/assets/social-media/discord.svg'
-                  alt='Discord'
-                  className='h-6 w-6'
-                />
-              </button>
-              <button className='flex w-full items-center justify-between rounded-lg border border-gray-700 bg-gray-700 px-4 py-3 transition-colors hover:border-gray-500'>
-                <span className='font-medium text-gray-100'>Continue with Github</span>
-                <img
-                  src='https://d36se1z1wvp1u2.cloudfront.net/img/assets/social-media/github.svg'
-                  alt='Github'
-                  className='h-6 w-6'
-                />
-              </button>
-            </div>
-
-     
-       
-
+        <button className='flex w-full items-center justify-between rounded-lg border border-gray-700 bg-gray-700 px-4 py-3 transition-colors hover:border-gray-500'>
+          <span className='font-medium text-gray-100'>Continue with Google</span>
+          <img
+            src='https://d36se1z1wvp1u2.cloudfront.net/img/assets/social-media/google.svg'
+            alt='Google'
+            className='h-6 w-6'
+          />
+        </button>
+        <button className='flex w-full items-center justify-between rounded-lg border border-gray-700 bg-gray-700 px-4 py-3 transition-colors hover:border-gray-500'>
+          <span className='font-medium text-gray-100'>Continue with Facebook</span>
+          <img
+            src='https://d36se1z1wvp1u2.cloudfront.net/img/assets/social-media/facebook.svg'
+            alt='Facebook'
+            className='h-6 w-6'
+          />
+        </button>
+        <button className='flex w-full items-center justify-between rounded-lg border border-gray-700 bg-gray-700 px-4 py-3 transition-colors hover:border-gray-500'>
+          <span className='font-medium text-gray-100'>Continue with Discord</span>
+          <img
+            src='https://d36se1z1wvp1u2.cloudfront.net/img/assets/social-media/discord.svg'
+            alt='Discord'
+            className='h-6 w-6'
+          />
+        </button>
+        <button className='flex w-full items-center justify-between rounded-lg border border-gray-700 bg-gray-700 px-4 py-3 transition-colors hover:border-gray-500'>
+          <span className='font-medium text-gray-100'>Continue with Github</span>
+          <img
+            src='https://d36se1z1wvp1u2.cloudfront.net/img/assets/social-media/github.svg'
+            alt='Github'
+            className='h-6 w-6'
+          />
+        </button>
+      </div>
     </>
   );
 }
