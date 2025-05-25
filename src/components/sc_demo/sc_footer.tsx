51a05fac8703bdf687c3cf3d2f126397d485a878
j@sharpi.sh <j@sharpi.sh>
2025-05-25 23:44:56 +1200

    fixed imports

--- src/components/sc_demo/sc_footer.tsx
+++ src/components/sc_demo/sc_footer.tsx
@@ -1,11 +1,12 @@
 'use client';
-import { SC_CONFIG } from "@/supacharger/supacharger-config";
-import  FacebookLogo  from "@/assets/images/socialAuthIcons/FacebookLogo.svg";
-import  MicrosoftLogo  from "@/assets/images/socialAuthIcons/MicrosoftLogo.svg";
-import  XLogo from "@/assets/images/socialAuthIcons/XLogo.svg";
-import  GoogleLogo  from "@/assets/images/socialAuthIcons/GoogleLogo.svg";
-import  LinkedinLogo  from "@/assets/images/socialAuthIcons/LinkedinLogo.svg";
-import SCSiteLogo from "./sc_site-logo";
+import FacebookLogo from '@/assets/images/socialAuthIcons/FacebookLogo.svg';
+import GoogleLogo from '@/assets/images/socialAuthIcons/GoogleLogo.svg';
+import LinkedinLogo from '@/assets/images/socialAuthIcons/LinkedinLogo.svg';
+import MicrosoftLogo from '@/assets/images/socialAuthIcons/MicrosoftLogo.svg';
+import XLogo from '@/assets/images/socialAuthIcons/XLogo.svg';
+import { SC_CONFIG } from '@/supacharger/supacharger-config';
+
+import SCSiteLogo from './sc_site-logo';
 
 export default function SCFooter() {
   return (
@@ -13,34 +14,32 @@ export default function SCFooter() {
       <div className='mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32'>
         <div className='xl:grid xl:grid-cols-3 xl:gap-8'>
           <div className='space-y-8'>
-          <div className="flex items-center">
-  <SCSiteLogo />
-  <div className="ml-3 font-bold">{SC_CONFIG.SITE_TITLE}</div>
-</div>
+            <div className='flex items-center'>
+              <SCSiteLogo />
+              <div className='ml-3 font-bold'>{SC_CONFIG.SITE_TITLE}</div>
+            </div>
 
-          
             <div className='flex gap-x-6'>
               <a href='#' className='text-gray-600 hover:text-gray-800'>
                 <span className='sr-only'>Facebook</span>
-                <FacebookLogo height="22" width="22"/>
+                <FacebookLogo height='22' width='22' />
               </a>
               <a href='#' className='text-gray-600 hover:text-gray-800'>
                 <span className='sr-only'>Microsoft</span>
-                <MicrosoftLogo height="22" width="22"/>
+                <MicrosoftLogo height='22' width='22' />
               </a>
               <a href='#' className='text-gray-600 hover:text-gray-800'>
                 <span className='sr-only'>X</span>
-                <XLogo height="22" width="22"/>
+                <XLogo height='22' width='22' />
               </a>
               <a href='#' className='text-gray-600 hover:text-gray-800'>
                 <span className='sr-only'>Google</span>
-                <GoogleLogo height="22" width="22"/>
+                <GoogleLogo height='22' width='22' />
               </a>
               <a href='#' className='text-gray-600 hover:text-gray-800'>
                 <span className='sr-only'>LinkedIn</span>
-                <LinkedinLogo height="22" width="22"/>
+                <LinkedinLogo height='22' width='22' />
               </a>
-              
             </div>
           </div>
           <div className='mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0'>
@@ -146,7 +145,9 @@ export default function SCFooter() {
           </div>
         </div>
         <div className='mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24'>
-          <p className='text-sm/6 text-gray-600'>&copy; {SC_CONFIG.SITE_TITLE}{" "}{new Date().getFullYear()}, All rights reserved.</p>
+          <p className='text-sm/6 text-gray-600'>
+            &copy; {SC_CONFIG.SITE_TITLE} {new Date().getFullYear()}, All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
