fab3727c723b93f9bf93a9a9e4c0b0f9d550363c
j@sharpi.sh <j@sharpi.sh>
2025-09-18 01:26:04 +0200

    updated stripe version

--- src/lib/stripe/stripe-admin.ts
+++ src/lib/stripe/stripe-admin.ts
@@ -3,5 +3,5 @@ import Stripe from 'stripe';
 import { getEnvVar } from '@/supacharger/utils/helpers';
 
 export const stripeAdmin = new Stripe(getEnvVar(process.env.STRIPE_SECRET_KEY, 'STRIPE_SECRET_KEY'), {
-  apiVersion: '2025-05-28.basil',
+  apiVersion: '2025-04-30.basil',
 });
